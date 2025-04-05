8222320414e27c442340f2fa68c3550ca80d2039
Bodi Ali <bodiali59@gmail.com>
2025-04-05 11:50:55 +0200

    Create a folder for Login page to contain css and component

---
+++ src/pages/LoginPage/Login.jsx
@@ -0,0 +1,43 @@
+import { useLocation } from "react-router";
+import Form from "../../components/Form/Form.jsx";
+
+export default function Login() {
+  const location = useLocation();
+
+  const flashMessage = location.state;
+
+  async function handleUserLogin(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/users/log-in`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const { token } = await res.json();
+
+    localStorage.setItem("token", token);
+  }
+
+  return (
+    <Form onSubmit={handleUserLogin}>
+      {flashMessage && <p>{flashMessage}</p>}
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="text" name="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </Form>
+  );
+}
