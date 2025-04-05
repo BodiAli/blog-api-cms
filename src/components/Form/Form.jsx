dd6c980fe424dbd71a08d6635c87b910fa9e1e99
Bodi Ali <bodiali59@gmail.com>
2025-04-05 11:49:47 +0200

    Create a folder for Form component to contain css and component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import logBlogIcon from "/images/the-log-blog-icon.svg";
+import styles from "./Form.module.css";
+
+export default function Form({ children, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>
+        <img src={logBlogIcon} alt="The Log Blog icon" />
+        The Log Blog
+      </h1>
+      {children}
+    </form>
+  );
+}
+
+Form.propTypes = {
+  children: PropTypes.element.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
