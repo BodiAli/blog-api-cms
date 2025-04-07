873f1982678f375fb716745d161dbaebd3587903
Bodi Ali <bodiali59@gmail.com>
2025-04-07 12:11:09 +0200

    Render ToastContainer in main.jsx to prevent it from unmounting hence showing the flash message

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 import routes from "./routes/routes.jsx";
 import "./reset.css";
 
@@ -9,5 +10,6 @@ const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer hideProgressBar={true} />
   </React.StrictMode>
 );
