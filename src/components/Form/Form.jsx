e45b2c3560fd07fe2540b86e45ca0c9e0385cde4
Bodi Ali <bodiali59@gmail.com>
2025-04-07 12:11:56 +0200

    Move The Log Blog hero to Login page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,9 @@
 import PropTypes from "prop-types";
-import logBlogIcon from "/images/the-log-blog-icon.svg";
 import styles from "./Form.module.css";
 
 export default function Form({ children, onSubmit }) {
   return (
     <form className={styles.form} onSubmit={onSubmit}>
-      <h1>
-        <img src={logBlogIcon} alt="The Log Blog icon" />
-        The Log Blog
-      </h1>
       {children}
     </form>
   );
