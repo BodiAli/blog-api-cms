1b066feb8be331483c01f19842d88e48e4bd041a
Bodi Ali <bodiali59@gmail.com>
2025-04-15 23:56:18 +0200

    Create tests for App component

---
+++ src/tests/unit/App.test.jsx
@@ -0,0 +1,46 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router";
+import routes from "../../routes/routes";
+
+const fakeToken = [
+  btoa(JSON.stringify({ alg: "HS256", type: "JWT" })),
+  btoa(JSON.stringify({ sub: "123" })),
+  "signature",
+].join(".");
+
+vi.stubGlobal("localStorage", {
+  getItem: vi.fn(() => fakeToken),
+});
+
+window.fetch = vi.fn(() => {
+  return Promise.resolve({
+    ok: true,
+    json: vi.fn(() =>
+      Promise.resolve({
+        user: {
+          firstName: "bodi",
+          lastName: "ali",
+          Profile: {
+            profileImgUrl: "imageUrl",
+          },
+        },
+      })
+    ),
+  });
+});
+
+describe("App component", () => {
+  test("Should render user profile and posts with expected management buttons", async () => {
+    const router = createMemoryRouter(routes);
+
+    let container;
+
+    await waitFor(() => {
+      const result = render(<RouterProvider router={router} />);
+
+      container = result.container;
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
