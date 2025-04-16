f85aa696a5c964ee5a0b68b576c3f318bf93a687
Bodi Ali <bodiali59@gmail.com>
2025-04-16 22:08:01 +0200

    Feat: Create a utility custom hook function to check if user is signed in by validating the token in the server and getting a response

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -1,7 +1,37 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const UserContext = createContext(null);
 
 export function useUser() {
   return useContext(UserContext);
+}
+
+export function useIsUserSignedIn() {
+  const [signedIn, setSignedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    async function fetchUser() {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/auth/validate`, {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        if (res.ok) {
+          setSignedIn(true);
+          return;
+        }
+      } catch {
+        console.error("Network error");
+        return;
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  return [signedIn, loading];
 }
