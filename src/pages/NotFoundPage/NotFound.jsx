4c42bd2d309ae22cb90511a10a25288c35a4d9c6
Bodi Ali <bodiali59@gmail.com>
2025-04-17 20:37:22 +0200

    Feat: Style page and render a link to head back to main app

--- src/pages/NotFoundPage/NotFound.jsx
+++ src/pages/NotFoundPage/NotFound.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router";
+import styles from "./NotFound.module.css";
+
 export default function NotFound() {
-  return <p>Looks like you are lost</p>;
+  return (
+    <main className={styles.main}>
+      <h1>404 Not found</h1>
+      <p>
+        Looks like you&apos;re lost! <Link to="/">Head back.</Link>
+      </p>
+    </main>
+  );
 }
