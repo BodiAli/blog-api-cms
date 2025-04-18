1dd862e2cd8d29a2cf0d9d4803e8915ad755d4ce
Bodi Ali <bodiali59@gmail.com>
2025-04-18 18:35:54 +0200

    Feat: create a test for onSubmit prop

--- src/tests/unit/Form.test.jsx
+++ src/tests/unit/Form.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Form from "../../components/Form/Form";
 
 describe("Form component", () => {
@@ -22,5 +23,24 @@ describe("Form component", () => {
 
     expect(screen.getByRole("heading", { level: 1, name: "heading 1" })).toBeInTheDocument();
     expect(screen.getByRole("heading", { level: 2, name: "heading 2" })).toBeInTheDocument();
+  });
+
+  test("should call onSubmit prop when form is submitted", async () => {
+    const user = userEvent.setup();
+    const handleSubmit = vi.fn((e) => {
+      e.preventDefault();
+    });
+
+    render(
+      <Form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </Form>
+    );
+
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+
+    await user.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledOnce();
   });
 });
