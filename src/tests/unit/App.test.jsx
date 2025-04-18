6f04d0291f3153ef52de03e5a978da9b71547f40
Bodi Ali <bodiali59@gmail.com>
2025-04-18 18:35:12 +0200

    Simplify mocks because mocking fetch is already implemented in ProtectedRoute test && Create 2 tests for navigation

--- src/tests/unit/App.test.jsx
+++ src/tests/unit/App.test.jsx
@@ -1,44 +1,55 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router";
+import userEvent from "@testing-library/user-event";
 import routes from "../../routes/routes";
+import { UserContext } from "../../utils/UserContext";
 
-const fakeToken = [
-  btoa(JSON.stringify({ alg: "HS256", type: "JWT" })),
-  btoa(JSON.stringify({ sub: "123" })),
-  "signature",
-].join(".");
+const user = {
+  firstName: "first name",
+  lastName: "last name",
+  Profile: { profileImgUrl: "imageUrl" },
+};
 
-vi.stubGlobal("localStorage", {
-  getItem: vi.fn(() => fakeToken),
-});
-
-window.fetch = vi.fn(() => {
-  return Promise.resolve({
-    ok: true,
-    json: vi.fn(() =>
-      Promise.resolve({
-        firstName: "bodi",
-        lastName: "ali",
-        Profile: {
-          profileImgUrl: "imageUrl",
-        },
-      })
-    ),
-  });
+vi.mock("../../routes/ProtectedRoute.jsx", async () => {
+  return {
+    default: vi.fn(({ children }) => {
+      return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+    }),
+  };
 });
 
 describe("App component", () => {
   test("Should render user profile and posts with expected management buttons", async () => {
     const router = createMemoryRouter(routes);
 
-    let container;
-
-    await waitFor(() => {
-      const result = render(<RouterProvider router={router} />);
-
-      container = result.container;
-    });
+    const { container } = render(<RouterProvider router={router} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test("navbar should navigate to '/create-post' path when Create post link is clicked", async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes);
+    render(<RouterProvider router={router} />);
+
+    const createPostLink = screen.getByRole("link", { name: "Create post" });
+
+    await user.click(createPostLink);
+
+    expect(router.state.location.pathname).toBe("/create-post");
+  });
+
+  test("navbar should navigate to '/' path when Manage posts link is clicked", async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes, { initialEntries: ["/create-post"] });
+    render(<RouterProvider router={router} />);
+
+    const managePostsLink = screen.getByRole("link", { name: "Manage posts" });
+
+    await user.click(managePostsLink);
+
+    expect(router.state.location.pathname).toBe("/");
   });
 });
