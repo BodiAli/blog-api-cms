30f31ef6334e6e9240a749a5d9dfaac25aa81683
Bodi Ali <bodiali59@gmail.com>
2025-04-21 00:23:09 +0200

    Feat: accept a disable prop to disable button while fetching data

--- src/components/FormButton/FormButton.jsx
+++ src/components/FormButton/FormButton.jsx
@@ -1,8 +1,12 @@
 import styles from "./FormButton.module.css";
 
-export default function FormButton({ children }) {
+export default function FormButton({ children, disabled }) {
   return (
-    <button className={styles.button} type="submit">
+    <button
+      disabled={disabled}
+      className={`${styles.button} ${disabled ? styles.disabled : ""}`}
+      type="submit"
+    >
       {children}
     </button>
   );
