7f19fd7a1ffa580f926780330673076975b6ce82
Bodi Ali <bodiali59@gmail.com>
2025-04-20 21:59:34 +0200

    Feat: Create error handler to handle unexpected errors thrown in '/' path

---
+++ src/components/ErrorHandler/ErrorHandler.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router";
+import styles from "./ErrorHandler.module.css";
+
+export default function ErrorHandler() {
+  const error = useRouteError();
+
+  return (
+    <div className={styles.error}>
+      <h2>Unexpected error occurred!</h2>
+      <p>{error.message}</p>
+    </div>
+  );
+}
