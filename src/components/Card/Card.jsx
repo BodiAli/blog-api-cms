4fea46ab928aef149d019249cebf58426f8b5eca
Bodi Ali <bodiali59@gmail.com>
2025-04-26 00:51:00 +0300

    Handle updating post's published state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { formatDistanceToNow } from "date-fns";
 import noImage from "../../assets/images/no-image.svg";
 import styles from "./Card.module.css";
 
-export default function Card({ post }) {
-  console.log(post);
+export default function Card({ post, onUpdate }) {
+  const [loading, setLoading] = useState(false);
+  const formattedCreatedAt = formatDistanceToNow(post.createdAt, { addSuffix: true, includeSeconds: true });
+
+  async function handlePublishPost() {
+    try {
+      setLoading(true);
+      console.log(post);
+
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${post.id}/publish`, {
+        method: "PATCH",
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ published: !post.published }),
+      });
+
+      console.log("RES", res);
+      const { msg, post: updatedPost } = await res.json();
+
+      onUpdate(updatedPost);
+      toast.success(msg);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div className={styles.card}>
       <div className={styles.text}>
         <div className={styles.textContainer1}>
           <p className={styles.title}>{post.title}</p>
-          <p className={styles.createdAt}>{post.createdAt}</p>
+          <p className={styles.createdAt}>{formattedCreatedAt}</p>
         </div>
         <div className={styles.textContainer2}>
           {post.Topics.length !== 0 &&
@@ -18,8 +49,18 @@ export default function Card({ post }) {
             })}
         </div>
         <div className={styles.textContainer3}>
-          <p>{post.likes}</p>
-          <p>{post.published ? "published" : "unpublished"}</p>
+          <p>
+            <strong>Likes: </strong>
+            {post.likes}
+          </p>
+          <label className={styles.switch}>
+            <button
+              disabled={loading}
+              onClick={handlePublishPost}
+              className={post.published ? styles.published : ""}
+            ></button>
+            <span className={`${styles.slider} ${styles.round}`}></span>
+          </label>
         </div>
       </div>
       <div className={styles.imageContainer}>
