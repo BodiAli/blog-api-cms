a5449da66f2f2caa4108a8d94bf281155f165e4f
Bodi Ali <bodiali59@gmail.com>
2025-04-26 03:21:37 +0300

    Create a modal to show selected post delete confirmation && extract fetching user posts to it's own function to call it again after deleting post

--- src/components/UserPosts/UserPosts.jsx
+++ src/components/UserPosts/UserPosts.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { toast } from "react-toastify";
 import Loader from "../Loader/Loader";
 import { useUser } from "../../utils/UserContext";
 import Card from "../Card/Card";
@@ -9,6 +10,8 @@ export default function UserPosts() {
   const [error, setError] = useState(null);
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const dialogRef = useRef(null);
 
   function handlePublishPost(updatedPost) {
     setPosts(
@@ -18,33 +21,73 @@ export default function UserPosts() {
     );
   }
 
-  useEffect(() => {
+  function showModal(post) {
+    return () => {
+      setSelectedPost(post);
+      dialogRef.current.showModal();
+    };
+  }
+
+  function closeModal() {
+    setSelectedPost(null);
+    dialogRef.current.close();
+  }
+
+  async function fetchUserPosts() {
     const queryString = new URLSearchParams(window.location.search);
     const page = queryString.get("page");
+    try {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/users/posts?page=${page}`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
 
-    async function fetchUserPosts() {
-      try {
-        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/users/posts?page=${page}`, {
-          headers: {
-            Authorization: localStorage.getItem("token"),
-          },
-        });
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch user posts");
-        }
-
-        const { posts: fetchedPosts } = await res.json();
-        setPosts(fetchedPosts);
-        // console.log(fetchedPosts);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+      if (!res.ok) {
+        throw new Error("Failed to fetch user posts");
       }
+
+      const { posts: fetchedPosts } = await res.json();
+      setPosts(fetchedPosts);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handlePostDelete() {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${selectedPost.id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch user posts");
+      }
+
+      await fetchUserPosts();
+
+      closeModal();
+      toast.success("Post deleted!");
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      fetchUserPosts();
     }
 
-    fetchUserPosts();
+    return () => {
+      ignore = true;
+    };
   }, []);
 
   if (error) throw new Error(error);
@@ -53,6 +96,17 @@ export default function UserPosts() {
 
   return (
     <main className={styles.main}>
+      <dialog className={styles.dialog} ref={dialogRef}>
+        <div className={styles.dialogContent}>
+          <p>
+            Are you sure you want to delete <strong>{selectedPost?.title}</strong>?
+          </p>
+          <div>
+            <button onClick={closeModal}>Cancel</button>
+            <button onClick={handlePostDelete}>Delete</button>
+          </div>
+        </div>
+      </dialog>
       <h1>
         {user.firstName} {user.lastName}&apos;s posts
       </h1>
@@ -62,7 +116,7 @@ export default function UserPosts() {
           <p className={styles.noPosts}>You have no posts yet</p>
         ) : (
           posts.map((post, i) => {
-            return <Card key={i} post={post} onUpdate={handlePublishPost} />;
+            return <Card key={i} post={post} onUpdate={handlePublishPost} requestPostDelete={showModal} />;
           })
         )}
       </div>
