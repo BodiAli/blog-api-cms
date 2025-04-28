436f641f18816990099b111d5cbff893b977adc7
Bodi Ali <bodiali59@gmail.com>
2025-04-28 10:36:43 +0300

    Feat: Create snapshot test for UserPost component

---
+++ src/tests/unit/UserPost.test.jsx
@@ -0,0 +1,49 @@
+import { render, waitFor } from "@testing-library/react";
+import { createRoutesStub } from "react-router";
+import UserPost from "../../components/UserPost/UserPost";
+
+const post = {
+  id: 1,
+  userId: 1,
+  title: "Post 1",
+  content: "Post content",
+  published: true,
+  likes: 5,
+  createdAt: "2025-04-27T03:03:02.292Z",
+  updatedAt: "2025-04-27T03:03:02.292Z",
+  imgUrl: null,
+  Topics: [
+    { id: 1, name: "Topic 1" },
+    { id: 2, name: "Topic 2" },
+  ],
+};
+
+window.fetch = vi.fn(() => {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    json: vi.fn(() => {
+      return Promise.resolve(post);
+    }),
+  });
+});
+
+const Stub = createRoutesStub([
+  {
+    path: "/users/posts/:postId",
+    Component: () => <UserPost />,
+  },
+]);
+
+describe("UserPost component", () => {
+  test("Should render post data", async () => {
+    let container;
+    await waitFor(() => {
+      const result = render(<Stub initialEntries={[`/users/posts/${post.id}`]} />);
+
+      container = result.container;
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
