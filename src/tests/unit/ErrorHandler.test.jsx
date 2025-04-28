8f7937947a033871043c4a6b4a39bed29c01ca36
Bodi Ali <bodiali59@gmail.com>
2025-04-28 12:13:36 +0300

    Feat: Test reload button

--- src/tests/unit/ErrorHandler.test.jsx
+++ src/tests/unit/ErrorHandler.test.jsx
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { createRoutesStub } from "react-router";
+import userEvent from "@testing-library/user-event";
 import ErrorHandler from "../../components/ErrorHandler/ErrorHandler";
 
 function ThrowErrorComponent() {
@@ -18,5 +19,25 @@ describe("ErrorHandler component", () => {
     const { container } = render(<Stub initialEntries={["/"]} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test("Should call reload page when reload button is clicked", async () => {
+    const user = userEvent.setup();
+
+    window.location = { reload: vi.fn() };
+
+    const Stub = createRoutesStub([
+      {
+        path: "/",
+        Component: ThrowErrorComponent,
+        ErrorBoundary: ErrorHandler,
+      },
+    ]);
+
+    render(<Stub initialEntries={["/"]} />);
+
+    await user.click(screen.getByRole("button"));
+
+    expect(window.location.reload).toHaveBeenCalledOnce();
   });
 });
