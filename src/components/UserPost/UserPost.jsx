4c0ec49f7c8b8769702c95d58ace2101e71d3788
Bodi Ali <bodiali59@gmail.com>
2025-04-28 07:55:51 +0300

    Fix: Navigate to '/' path if an error occurred and if post is not found throw error with error message received from server && add optional chaining if post is null to not throw errors and for third party packages only call methods if post is not null to not call them with invalid values

--- src/components/UserPost/UserPost.jsx
+++ src/components/UserPost/UserPost.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import parse from "html-react-parser";
 import { toast } from "react-toastify";
 import { format, formatDistanceToNow } from "date-fns";
@@ -11,6 +11,7 @@ export default function UserPost() {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchUserPost() {
@@ -18,6 +19,10 @@ export default function UserPost() {
         const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${postId}`);
 
         if (!res.ok) {
+          if (res.status === 404) {
+            const { error } = await res.json();
+            throw new Error(error);
+          }
           throw new Error("Failed to fetch post");
         }
 
@@ -28,50 +33,51 @@ export default function UserPost() {
         setPost(post);
       } catch (error) {
         toast.error(error.message);
+        navigate("/", { viewTransition: true });
       } finally {
         setLoading(false);
       }
     }
 
     fetchUserPost();
-  }, [postId]);
+  }, [postId, navigate]);
 
   if (loading) return <Loader />;
 
   return (
     <main className={styles.main}>
       <div className={styles.textContainer1}>
-        <h2 className={styles.title}>{post.title}</h2>
+        <h2 className={styles.title}>{post?.title}</h2>
         <p className={styles.date}>
           <strong>Created at: </strong>
-          {format(post.createdAt, "y/M/d, H:m:s")}
+          {post && format(post.createdAt, "y/M/d, H:m:s")}
         </p>
         <p className={styles.date}>
           <strong>Last updated: </strong>
-          {formatDistanceToNow(post.updatedAt, { addSuffix: true, includeSeconds: true })}
+          {post && formatDistanceToNow(post.updatedAt, { addSuffix: true, includeSeconds: true })}
         </p>
       </div>
       <div className={styles.textContainer2}>
         <p className={styles.likes}>
-          <strong>Likes:</strong> {post.likes}
+          <strong>Likes:</strong> {post?.likes}
         </p>
-        <p className={styles.published}>{post.published ? "Published" : "Unpublished"}</p>
+        <p className={styles.published}>{post?.published ? "Published" : "Unpublished"}</p>
       </div>
       <div className={styles.textContainer3}>
         <p>Topics:</p>
         <div>
-          {post.Topics.length === 0 ? (
+          {post?.Topics.length === 0 ? (
             <span>This post has no topics</span>
           ) : (
-            post.Topics.map((topic, i) => {
+            post?.Topics.map((topic, i) => {
               return <p key={i}>{topic.name}</p>;
             })
           )}
         </div>
       </div>
       <div className={styles.postContent}>
-        <img src={post.imgUrl ? post.imgUrl : noImage} alt="Post cover" />
-        <div className={styles.postContentHtml}>{parse(post.content)}</div>
+        <img src={post?.imgUrl ? post?.imgUrl : noImage} alt="Post cover" />
+        <div className={styles.postContentHtml}>{post && parse(post.content)}</div>
       </div>
 
       <div className={styles.commentsContainer}></div>
